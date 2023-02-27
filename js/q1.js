5b825133fdd8a5fee2aac09ba18f30151f0d449f
tangjiahui <tjhaaa@163.com>
2023-02-27 11:53:13 +0800

    fix: 修正js/q1

--- js/q1.js
+++ js/q1.js
@@ -8,19 +8,19 @@ const timeoutArr = [1000, 3000, 2000, 9000, 8000, 7000, 5000, 3000, 1000, 700]
 const requestArr = timeoutArr.map(timeout => () => query(timeout))
 concurrentRequest2(requestArr, 3)
 
-function query (timeout) {
-    return new Promise (resolve => {
+function query(timeout) {
+    return new Promise(resolve => {
         setTimeout(() => resolve(timeout), timeout)
     })
 }
 
 // 方法一：使用Promise.race
-function concurrentRequest (requestArr = [], concurrent = 3) {
+function concurrentRequest(requestArr = [], concurrent = 3) {
     let i = concurrent
     const concurrentArr = requestArr.slice(0, concurrent).map(createNewRequest)
     run(concurrentArr)
 
-    function createNewRequest (request) {
+    function createNewRequest(request) {
         const newRequest = request().then(res => {
             concurrentArr.splice(concurrentArr.indexOf(newRequest), 1)
             return res
@@ -28,32 +28,31 @@ function concurrentRequest (requestArr = [], concurrent = 3) {
         return newRequest
     }
 
-    function run (concurrentArr) {
+    function run(concurrentArr) {
         if (!concurrentArr.length) return
         Promise.race(concurrentArr).then(res => {
             console.log('弹出: ', res)
-            if (i >= requestArr.length) {
-                run(concurrentArr)
-            } else {
+            if (i < requestArr.length) {
                 concurrentArr.push(createNewRequest(requestArr[i++]))
-                run(concurrentArr)
             }
+            run(concurrentArr)
         })
     }
 }
 
 // 方法二：删除请求时新增
-function concurrentRequest2 (requestArr = [], concurrent = 3) {
+function concurrentRequest2(requestArr = [], concurrent = 3) {
     let i = concurrent
     const concurrentArr = requestArr.slice(0, concurrent).map(createNewRequest)
 
-    function createNewRequest (request) {
+    function createNewRequest(request) {
         const newRequest = request().then(res => {
             console.log('弹出: ', res)
             concurrentArr.splice(concurrentArr.indexOf(newRequest), 1)
 
-            if (i >= requestArr.length) return
-            concurrentArr.push(createNewRequest(requestArr[i++]))
+            if (i < requestArr.length) {
+                concurrentArr.push(createNewRequest(requestArr[i++]))
+            }
             return res
         })
         return newRequest
