aa1544d64cbd4c834888296a2d2afdf2c03e5021
tangjiahui-cn <tjhaaa@163.com>
2023-03-24 19:24:27 +0800

    add: Array._reduce、Array._reduceRight

--- handWriteSource/IArrayPrototype.js
+++ handWriteSource/IArrayPrototype.js
@@ -21,6 +21,21 @@ Array.prototype._map = function (fn) {
     return result
 }
 
+Array.prototype._reduce = function (fn, defaultValue) {
+    let result = defaultValue
+    for (let i = 0; i < this.length; i++) {
+        result = fn?.(result, this[i], this)
+    }
+    return result
+}
+
+Array.prototype._reduceRight = function (fn, defaultValue) {
+    let result = defaultValue
+    for (let i = this.length - 1; i >= 0; i--) {
+        result = fn?.(result, this[i], this)
+    }
+    return result
+}
 
 // 测试
 const list = [
@@ -36,3 +51,5 @@ list._forEach(x => console.log('forEach -> ', x))
 // map
 const mapList = list._map(x => ({...x, id: 'map_' + x?.id}))
 console.log('mapList -> ', mapList)
+console.log('reduce -> ', list._reduce((result, cur) => [...result, cur?.id], [])) // [1,2,3,4,5]
+console.log('reduce -> ', list._reduceRight((result, cur) => [...result, cur?.id], [])) // [5,4,3,2,1]
