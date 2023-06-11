3e40aea206708f947e118ac7fa3f10ad78e94129
tangjiahui <tjhaaa@163.com>
2023-06-11 22:15:31 +0800

    add(设计模式):原型模式.

---
+++ pattern/1.prototype.js
@@ -0,0 +1,18 @@
+// 1、原型模式
+// 将 对象作为构造函数的原型，以此复制对象上的所有属性。
+
+
+const obj = {
+  name: '111',
+  show () {
+    console.log(this.name)
+  }
+}
+
+const fn = function () {}
+fn.prototype = obj;
+
+new fn().show();
+
+// 或者
+Object.create(obj).show();
