b802ef39bfefc07cd2b456378921a0180dbd82d7
tangjiahui <tjhaaa@163.com>
2023-07-19 21:01:51 +0800

    fix(pattern): 修复单例模式

--- pattern/3.single.js
+++ pattern/3.single.js
@@ -1,16 +1,36 @@
 /**
  * 单例模式
- * 
- * 解释：只需要一个对象
+ *
+ * 解释：确保只有一个实例，提供全局访问
  */
 
 class Single {
-    ins = null;
+  ins = null;
 
-    static getIns () {
-        if (this.ins) return this.ins;
-        return this.ins = {};
-    }
+  static getIns() {
+    if (this.ins) return this.ins;
+    return (this.ins = {});
+  }
 }
 
-console.log(Single.getIns() === Single.getIns()) // true
+console.log(Single.getIns() === Single.getIns()); // true
+
+// 提取公共单例逻辑
+function MyIns(name) {
+  return {
+    show() {
+      console.log("->", name);
+    },
+  };
+}
+
+function getSingle(fn) {
+  var result;
+  return function () {
+    return result || (result = fn.apply(this, arguments));
+  };
+}
+
+const CreateIns = getSingle(MyIns);
+console.log(CreateIns("aa") === CreateIns("bb")); // true
+CreateIns("cc").show(); // 'aa'
